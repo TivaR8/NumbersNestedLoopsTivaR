36aedaef352e2f3f399de7de4606db0c178c31aa
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-04-13 13:10:05 -0400

    Initial commit

---
+++ NumbersNestedLoopsTivaR/NumbersNestedLoopsForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 13-04-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #25 - Numbers Nested Loops
+ * This program...
+*/
+
+namespace NumbersNestedLoopsTivaR
+{
+    public partial class frmNumbersNestedLoops : Form
+    {
+        public frmNumbersNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Declare Variables
+            int firstNumber;
+            int secondNumber;
+
+            // Clear the list box
+            this.lstNumbersOutput.Items.Clear();
+
+            // Loop through the first number 
+            for (firstNumber = 0; firstNumber <= 10; firstNumber++)
+            {
+                //Now to loop through the next number
+            }
+        }
+    }
+}
